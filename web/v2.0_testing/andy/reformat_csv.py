06ee788952af84355086d3de9cd94da171cc37bc
avnewman <andrewvnewman@gmail.com>
2021-04-26 13:16:10 -0400

    update reformat_csv

--- web/v2.0_testing/andy/reformat_csv.py
+++ web/v2.0_testing/andy/reformat_csv.py
@@ -27,18 +27,21 @@ df2.to_csv('rterg_summary_M65.csv')
 import pygsheets
 
 # authorization
-gc = pygsheets.authorize(service_file='./rterg-cat-c9494a26a4db.json')
+#gc = pygsheets.authorize(service_file='./rterg-cat-c9494a26a4db.json')
+# moved out of shared directory as I accidently pushed the old key (needs to remain private)
+gc = pygsheets.authorize(service_file='/Users/anewman/Documents/GoogleCloud/keys/rterg-cat-0f0c1afdc799.json')
 
 # open remote google sheet
 sh = gc.open('RTerg_cat')  # should match name I assume
 
 wks = sh[0] # first sheet
-
-wks.set_dataframe(df2,(0,0))  # write everything to sheet
+wks.resize(rows=len(df))  # by default google sheets are only 1000 rows.  Need to resize to allow new data
+wks.set_dataframe(df,(0,0))  # write everything to sheet
 
 #wks.update_value('A1',"Eventname") # update individual value
 
 # need to create new sheet first (within googlesheets)
 #  too may have troubles the first few times, as you may need to go into the bottom of google
 #   sheets to add more lines
-sh[1].set_dataframe(df,(0,0))  # write full dataset to second sheet
+sh[1].resize(rows=len(df2))
+sh[1].set_dataframe(df2,(0,0))  # write full dataset to second sheet
